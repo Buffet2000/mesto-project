e9ef2c990451919d64a2b8abcd6fbd12eff5f6f8
Buffet2000 <buffet2000@gmail.com>
2022-07-04 09:49:18 +0300

    Image card adding working.

--- script/index.js
+++ script/index.js
@@ -1,13 +1,13 @@
-let editButton = document.querySelector('.profile__button-edit');
-let editProfile = document.getElementById('edit_profile');
-let closeProfile = document.getElementById('close_profile');
+const editButton = document.querySelector('.profile__button-edit');
+const editProfile = document.getElementById('edit_profile');
+const closeProfile = document.getElementById('close_profile');
 
-let addButton = document.querySelector('.profile__button-add');
-let addPlace = document.getElementById('add_new_place');
-let closePlace = document.getElementById('close_new_place');
+const addButton = document.querySelector('.profile__button-add');
+const addPlace = document.getElementById('add_new_place');
+const closePlace = document.getElementById('close_new_place');
 
-let images = document.querySelector('.elements');
-let createButton = document.getElementById('create');
+const images = document.querySelector('.elements');
+const createButton = document.getElementById('create');
 
 /*Открывание/закрывание окон*/
 function edit() {
@@ -26,9 +26,32 @@ function close_place() {
 	addPlace.classList.remove('popup_opened');
 }
 
-editButton.addEventListener ('click', edit);
-closeProfile.addEventListener ('click', close_profile);
+editButton.addEventListener('click', edit);
+closeProfile.addEventListener('click', close_profile);
 addButton.addEventListener('click', add);
 closePlace.addEventListener('click', close_place);
 
+/*Добавление фотографии*/
+function addImage(placeValue, linkValue) {
 
+  const cardTemplate = document.querySelector('#element-template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+  cardElement.querySelector('.element__title').textContent = placeValue;
+  cardElement.querySelector('.element__image').src = linkValue;
+
+  images.append(cardElement);
+};
+
+/*Создать карточку с фото*/
+createButton.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	const imageTitle = document.querySelector('.popup__input_type_place-name');
+  const imagelink = document.querySelector('.popup__input_type_place-link');
+
+  addImage(imageTitle.value, imagelink.value);
+
+  imageTitle.value = '';
+  imagelink.value = '';
+	close_place();
+});
